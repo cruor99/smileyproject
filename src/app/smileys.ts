ab15f70628427c83c13e4df9836446b0362dcc1b
Cruor99 <kliknes@gmail.com>
2017-05-21 17:26:59 +0200

    fixed object to properly map

--- src/app/smileys.ts
+++ src/app/smileys.ts
@@ -1,15 +1,27 @@
 
 export class Smileys{
-    objectid: string;
+    tilsynsobjektid: string;
     orgnummer: number;
-    name: string;
-    address: string;
-    postnumber: number;
-    area: string;
-    checkerid: string;
-    caseref: string;
+    navn: string;
+    adrlinje1: string;
+    postnr: number;
+    poststed: string;
+    tilsynid: string;
+    sakref: string;
     status: number;
-    date: string;
-    overallGrade: number;
-    visit_type: string;
+    dato: string;
+    total_karakter: number;
+    tilsynsbesoektype: string;
+    tema1_no: string;
+    tema1_nn: string;
+    karakter1: number;
+    tema2_no: string;
+    tema2_nn: string;
+    karakter2: number;
+    tema3_no: string;
+    tema3_nn: string;
+    karakter3: number;
+    tema4_no: string;
+    tema4_nn: string;
+    karakter4: string;
 }
